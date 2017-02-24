52c56c767ad48f1addaa6eef212c2883d0cff3de
akalola <avk43@scarletmail.rutgers.edu>
2017-02-24 14:02:17 -0500

    Add files via upload

---
+++ AdventureModel.java
@@ -0,0 +1,49 @@
+/*
+ * @author oranges
+ * @version 1.0
+This class:
+•	Creates the Rooms, the Things, and the Adventurer.
+•	Connects the Rooms with "paths" to other Rooms.
+•	Places Things in the Rooms.
+•	Places the Adventurer in some Room.
+•	Accepts commands from the player, and executes them.
+	o	As commands are entered, they should be copied to the main text area.
+	o	The method that executes commands should return a String to be displayed in the main text area.
+*/
+
+
+package adventure;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Random;
+
+
+public class AdventureModel {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// creating methods for the different rooms for part 2
+		
+		public static void hallway(){
+			
+		}
+		
+		public static void kitchen(){
+			
+		}
+		public static void diningRoom(){
+			
+		}
+		public static void bathroom(){
+			
+		}
+		
+		
+		
+		
+	}
+	
+}
+			
---
+++ Adventurer.java
@@ -0,0 +1,71 @@
+/*
+ * @author oranges
+ * @version 1.0
+An adventurer has:
+•	A location (some room).
+•	An inventory (the things being carried).  When the player executes the “take thing” command, the item should be added to the inventory.
+An adventurer can:
+•	Move from room to room.
+•	Carry a number of objects.
+•	Pick up, drop, look at, and use various objects.
+ */
+
+package adventure;
+
+import java.util.ArrayList;
+
+public class Adventurer {
+	
+	//import arraylist called backpack that will be called in TextAdventure
+	public static ArrayList<String> backpack = new ArrayList<String>();
+
+	private boolean bronzekey = false; // all the items that you will need for the game 
+	private boolean silverkey = false;
+	private boolean goldkey = false;
+	private boolean flashlight = false;
+	private boolean knife = false;
+	
+	private boolean watch = false;
+	
+	//create a player identity
+	String name;
+	public void setName(String n){
+	name = n;
+	}
+	public String getName(){
+	return name;
+	}
+	
+	
+	public boolean bronzekey(){
+		return true ;
+	}
+	
+	public boolean silverkey(){
+		return true;
+	}
+	public boolean goldkey(){
+		return true;
+	}
+	public boolean flashlight(){
+		return true;
+	}
+	public boolean knife(){
+		return true;
+	}
+	public boolean watch(){
+		return true;
+	}
+	
+}
+
+
+	
+
+	
+	
+    
+
+
+
+
